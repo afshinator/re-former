ba15365da4385d0d9a3b4c2251259fbf83f38492
Afshin Mokhtari <afshinator@hotmail.com>
2014-05-13 02:43:14 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## re-former
+
+### Projects: [Forms from theOdinProject](http://www.theodinproject.com/ruby-on-rails/forms)
+
+Project 1: Bare Metal Forms and Helpers
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReFormer::Application.config.secret_key_base = 'a750fc9f6d4b8360781ec89c1633a1e007fd03068042339c5d9a075a3aceefb15b0fa0081b3c67699c42fcd66f8031be7c2ec282f2fc60daf3527d702457945b'
