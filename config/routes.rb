dae37cac4fcc4dc746b2798060391fdcf648081e
Afshin Mokhtari <afshinator@hotmail.com>
2014-05-18 23:10:51 +0000

    finish the exercise

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ReFormer::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit, :update, :index]
   
   
   # You can have the root of your site routed with "root"
